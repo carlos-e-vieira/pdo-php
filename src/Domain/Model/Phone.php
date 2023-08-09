9ba77ad6647bdd5f32797a96bc94517dc359781d
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-08-09 19:58:12 -0300

    added phone model

---
+++ src/Domain/Model/Phone.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Model;
+
+class Phone
+{
+    private ?int $id;
+    private string $areaCode;
+    private string $number;
+
+    public function __construct(
+        ?int $id,
+        string $areaCode,
+        string $number,
+    ) {
+        $this->id = $id;
+        $this->areaCode = $areaCode;
+        $this->number = $number;
+    }
+
+    public function formattedPhone(): string
+    {
+        return "($this->areaCode) $this->number";
+    }
+}
--- src/Domain/Model/Student.php
+++ src/Domain/Model/Student.php
@@ -12,6 +12,8 @@ class Student
     private ?int $id;
     private string $name;
     private DateTimeInterface $birthDate;
+    /** @var Phone[] */
+    private array $phones = [];
 
     public function __construct(?int $id, string $name, DateTimeInterface $birthDate)
     {
@@ -54,5 +56,16 @@ class Student
         return $this->birthDate
             ->diff(new DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhone(Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    /** @var Phone[] */
+    public function phones(): array
+    {
+        return $this->phones;
     }
 }
