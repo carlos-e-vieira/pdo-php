d2c63ac0a8e738ddb17851f0e5820d2189d4462f
Carlos Vieira <carlosportalsocial@gmail.com>
2023-08-10 10:49:03 -0300

    update code in trade

--- src/Domain/Repository/StudentRepository.php
+++ src/Domain/Repository/StudentRepository.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace App\Domain\Repository;
 
 use App\Domain\Model\Student;
+use DateTimeInterface;
 
 interface StudentRepository
 {
     public function allStudents(): array;
-    public function studentsBirthAt(\DateTimeInterface $birthDate): array;
+
+    public function studentsWithPhones(): array;
+
+    public function studentsBirthAt(DateTimeInterface $birthDate): array;
+
     public function save(Student $student): bool;
+
     public function remove(Student $student): bool;
 }
